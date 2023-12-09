740d0da3ebe0a5df96fb9acfc0de378a4c786d05
eebbesen <eebbesen.git@gmail.com>
2023-12-09 08:17:00 -0600

    app-wide formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
