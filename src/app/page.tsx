39102bdd4b4923a1183f3b35a1ee644ea92af45a
eebbesen <eebbesen.git@gmail.com>
2023-12-09 08:39:11 -0600

    adding radio buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Page() {
   }
 
   return (
-    <main className="">
-      <div className="mx-auto p-1 bg-amber-200">
+    <main className="mx-auto p-1 bg-amber-200">
+      <div className="">
         <div className="flex bg-amber-300">
           <button className=""
             onClick={handleSubmit}
@@ -38,18 +38,55 @@ export default function Page() {
             onChange={(e) => {setWorkingUrl(e.target.value)}}
             ></input>
         </div>
-        <div>
-          <table className="bg-amber-400">
-            <thead>
-              <DataHeaderRow data={data}/>
-            </thead>
-            <tbody className="">
-              {data.map((r, i) => (
-                <DataRowMemo key={ r.id ? r.id : Date.now() * i } row={r}/>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <fieldset className="flex">
+          <div className="py-3 bg-amber-600">
+            <div className="flex gap-4">
+              <legend className="block">
+                Vendor
+              </legend>
+              <div className="flex items-center">
+                <input
+                  id="arcgis"
+                  name="datasource-type"
+                  type="radio"
+                  value="arcgis"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='datasource-type-error'
+                />
+                <label htmlFor="arcgis" className="px-3 block text-sm font-medium">
+                  ArcGIS
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="soctrata"
+                  name="datasource-type"
+                  type="radio"
+                  value="socrata"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='datasource-type-error'
+                />
+                <label htmlFor="socrata" className="px-3 block text-sm font-medium">
+                  Socrata
+                </label>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+
+
+      <div>
+        <table className="bg-amber-400">
+          <thead>
+            <DataHeaderRow data={data}/>
+          </thead>
+          <tbody className="">
+            {data.map((r, i) => (
+              <DataRowMemo key={ r.id ? r.id : Date.now() * i } row={r}/>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
   )
