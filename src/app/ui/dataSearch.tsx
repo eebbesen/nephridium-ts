898624c8101a35ebd70686d7ba4188f21fcb8bda
eebbesen <eebbesen.git@gmail.com>
2023-12-10 08:07:58 -0600

    component tests

--- src/app/ui/dataSearch.tsx
+++ src/app/ui/dataSearch.tsx
@@ -9,6 +9,8 @@ export default function DataSearch({
     <div id="search-components" className="space-y-3">
       <div id="search-url" className="grid gap-2 sm:grid-cols-2 md:grid-cols-9">
         <button
+          id="search-button"
+          name="search-button"
           className="col-start-1 col-end-2 rounded-md bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-400"
           onClick={handleSubmit}
         >
@@ -21,6 +23,7 @@ export default function DataSearch({
           className="col-start-2 col-end-10 rounded-md"
           value={workingUrl}
           placeholder="Enter URL to Dataset"
+          aria-label="URL to dataset"
           onChange={(e) => {
             setWorkingUrl(e.target.value)
           }}
