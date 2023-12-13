a6f1ea6ee09e0851a34842d65a7653dc8d54f711
eebbesen <eebbesen.git@gmail.com>
2023-12-13 13:52:01 -0600

    selectable columns

--- src/app/ui/dataHeaderRow.tsx
+++ src/app/ui/dataHeaderRow.tsx
@@ -1,32 +1,39 @@
 const transformer = require('../lib/util/transformer')
 
-let selected: string[] = []
-
-// todo: refactor to share state up the tree ina React way
-// todo: investigate using tailwind instead of direct styles
-function handleClick (target: string, e: any ): void {
-  const i: number = selected.indexOf(target)
-  if (i < 0) {
-    selected = [...selected, target]
-    e.target.style.color = 'white'
-    e.target.style.background = 'black'
-  } else {
-    selected = (selected.toSpliced(i, 1))
-    e.target.style.color = 'black'
-    e.target.style.background = 'rgb(147 197 253)'
-  }
-  console.log('selected: ', selected)
-}
-
-export default function DataHeaderRow({ columns }: any) {
+export default function DataHeaderRow({ columns, selected, setSelected }: any) {
   if (columns.length === 0) {
     return
+  }
+
+  // todo: refactor to share state up the tree ina React way
+  // todo: investigate using tailwind instead of direct styles
+  const handleClick = (target: string, e: any): void => {
+    const i: number = selected.indexOf(target)
+    if (i < 0) {
+      setSelected([...selected, target])
+      e.target.classList.replace('text-black', 'text-white')
+      e.target.classList.replace('bg-blue-300', 'bg-black')
+    } else {
+      setSelected(selected.toSpliced(i, 1))
+      e.target.classList.replace('text-white', 'text-black')
+      e.target.classList.replace('bg-black', 'bg-blue-300')
+    }
   }
 
   return (
     <tr key={1}>
       {columns.map((c: string) => (
-        <th className="pe-1 ps-1" onClick={(e) => {handleClick(c, e)}} key={c}>
+        <th
+          className={`pe-1 ps-1 ${
+            selected.indexOf(c) < 0
+              ? 'bg-blue-300 text-black'
+              : 'bg-black text-white'
+          }`}
+          onClick={(e) => {
+            handleClick(c, e)
+          }}
+          key={c}
+        >
           {transformer.transform(c)}
         </th>
       ))}
--- src/app/ui/dataTable.tsx
+++ src/app/ui/dataTable.tsx
@@ -2,32 +2,57 @@ import { useState } from 'react'
 import DataHeaderRow from './dataHeaderRow'
 import { DataRowMemo } from './dataRow'
 
-export default function DataTable({data, columns}: any) {
+export default function DataTable({ data, columns }: any) {
   const [selected, setSelected] = useState([])
 
+  // todo: consider a ref here
+  const toggleAll = () => {
+    if (selected.length === 0) {
+      setSelected(columns.slice())
+    } else {
+      setSelected([])
+    }
+  }
+
   return (
-    <div
-      id="data-table"
-      className="relative w-full overflow-x-auto sm:rounded-sm"
-    >
-      <table className="border-separate md:table ">
-        <thead className="bg-blue-300 text-xs font-bold uppercase">
-          <DataHeaderRow
-            columns={columns}
-            selected={selected}
-            setSelected={setSelected} />
-        </thead>
-        <tbody className="align-text-top text-xs">
-          {data.map((r: any, i: number) => (
-            <DataRowMemo
-              key={r['id'] ? r['id'] : Date.now() * i}
-              row={r}
-              data={data}
+    <>
+      <div
+        id="toggle-all-div"
+        className="grid gap-2 sm:grid-cols-2 md:grid-cols-9"
+      >
+        <button
+          id="toggle-all"
+          name="toggle-all"
+          className="col-start-1 col-end-2 rounded-md bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-400"
+          onClick={toggleAll}
+        >
+          {selected.length > 0 ? 'Deselect All' : 'Select All'}
+        </button>
+      </div>
+      <div
+        id="data-table"
+        className="relative w-full overflow-x-auto sm:rounded-sm"
+      >
+        <table className="border-separate md:table ">
+          <thead className="bg-blue-300 text-xs font-bold uppercase">
+            <DataHeaderRow
               columns={columns}
+              selected={selected}
+              setSelected={setSelected}
             />
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody className="align-text-top text-xs">
+            {data.map((r: any, i: number) => (
+              <DataRowMemo
+                key={r['id'] ? r['id'] : Date.now() * i}
+                row={r}
+                data={data}
+                columns={columns}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
