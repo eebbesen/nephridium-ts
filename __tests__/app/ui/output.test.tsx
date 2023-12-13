b2a762d0a608f31fac50f1bca03263ccfa9c07a4
eebbesen <eebbesen.git@gmail.com>
2023-12-13 16:49:23 -0600

    prettier

--- __tests__/app/ui/output.test.tsx
+++ __tests__/app/ui/output.test.tsx
@@ -4,35 +4,53 @@ import '@testing-library/jest-dom'
 
 describe('Output', () => {
   it('renders URL with all elements', () => {
-    render(<Output columns={['id', 'date', 'description', 'address']}
-      selected={['id', 'date', 'description']}
-      selectedDateColumn={'date'}
-      url={'https://some.domain.biz.json?$limit=3'} />)
+    render(
+      <Output
+        columns={['id', 'date', 'description', 'address']}
+        selected={['id', 'date', 'description']}
+        selectedDateColumn={'date'}
+        url={'https://some.domain.biz.json?$limit=3'}
+      />,
+    )
 
     const urlFragment = screen.getByText(/domain.biz/).textContent
 
-    expect(urlFragment).toEqual('&time_column=date&to_remove=address&url=https://some.domain.biz.json')
+    expect(urlFragment).toEqual(
+      '&time_column=date&to_remove=address&url=https://some.domain.biz.json',
+    )
   })
 
   it('renders URL without selectedDateColumn', () => {
-    render(<Output columns={['id', 'date', 'description', 'address']}
-      selected={['id', 'date', 'description']}
-      selectedDateColumn={''}
-      url={'https://some.domain.biz.json?$limit=3'} />)
+    render(
+      <Output
+        columns={['id', 'date', 'description', 'address']}
+        selected={['id', 'date', 'description']}
+        selectedDateColumn={''}
+        url={'https://some.domain.biz.json?$limit=3'}
+      />,
+    )
 
     const urlFragment = screen.getByText(/domain.biz/).textContent
 
-    expect(urlFragment).toEqual('&to_remove=address&url=https://some.domain.biz.json')
+    expect(urlFragment).toEqual(
+      '&to_remove=address&url=https://some.domain.biz.json',
+    )
   })
 
   it('renders URL without removed columns', () => {
-    render(<Output columns={['id', 'date', 'description', 'address']}
-      selected={['id', 'date', 'description', 'address']}
-      selectedDateColumn={'date'}
-      url={'https://some.domain.biz.json?$limit=3'} />)
+    render(
+      <Output
+        columns={['id', 'date', 'description', 'address']}
+        selected={['id', 'date', 'description', 'address']}
+        selectedDateColumn={'date'}
+        url={'https://some.domain.biz.json?$limit=3'}
+      />,
+    )
 
     const urlFragment = screen.getByText(/domain.biz/).textContent
 
-    expect(urlFragment).toEqual('&time_column=date&url=https://some.domain.biz.json')
+    expect(urlFragment).toEqual(
+      '&time_column=date&url=https://some.domain.biz.json',
+    )
   })
 })
