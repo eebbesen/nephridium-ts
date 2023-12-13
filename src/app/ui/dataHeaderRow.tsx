49d3185aec119dc0083f19516194348a94066c47
eebbesen <eebbesen.git@gmail.com>
2023-12-13 08:59:56 -0600

    change styles on select

--- src/app/ui/dataHeaderRow.tsx
+++ src/app/ui/dataHeaderRow.tsx
@@ -2,12 +2,17 @@ const transformer = require('../lib/util/transformer')
 
 let selected: string[] = []
 
-function handleClick (target: string ): void {
+// todo: refactor to share state up the tree ina React way
+function handleClick (target: string, e: any ): void {
   const i: number = selected.indexOf(target)
   if (i < 0) {
     selected = [...selected, target]
+    e.target.style.color = 'white'
+    e.target.style.background = 'black'
   } else {
     selected = (selected.toSpliced(i, 1))
+    e.target.style.color = 'black'
+    e.target.style.background = 'rgb(147 197 253)'
   }
   console.log('selected: ', selected)
 }
@@ -20,8 +25,7 @@ export default function DataHeaderRow({ columns }: any) {
   return (
     <tr key={1}>
       {columns.map((c: string) => (
-        // eslint-disable-next-line react/jsx-key
-        <th className="pe-1 ps-1" onClick={() => {handleClick(c)}} key={c}>
+        <th className="pe-1 ps-1" onClick={(e) => {handleClick(c, e)}} key={c}>
           {transformer.transform(c)}
         </th>
       ))}
