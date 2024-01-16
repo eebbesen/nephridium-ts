ab66f8cde9a6b1154e1b35dc7444b62038216f0f
eebbesen <eebbesen.git@gmail.com>
2024-01-16 09:36:20 -0600

    more column toggle testing

--- __tests__/app/ui/dataHeaderRow.test.tsx
+++ __tests__/app/ui/dataHeaderRow.test.tsx
@@ -16,7 +16,7 @@ describe('DataHeaderRow', () => {
     expect(() => screen.getAllByRole('columnheader')).toThrow('Unable to find')
   })
 
-  it('renders colummns when selected', () => {
+  it('renders colummn headers properly when selected', () => {
     render(
       <DataHeaderRow
         columns={['id', 'address']}
@@ -25,12 +25,15 @@ describe('DataHeaderRow', () => {
       />,
     )
     const addressHeader = screen.getAllByRole('columnheader')[0]
+    const idHeader = screen.getAllByRole('columnheader')[1]
 
     expect(addressHeader.className).toEqual('pe-1 ps-1 bg-black text-white')
+    expect(idHeader.className).toEqual('pe-1 ps-1 bg-black text-white')
 
     addressHeader.click()
 
     expect(addressHeader.className).toEqual('pe-1 ps-1 bg-blue-300 text-black')
+    expect(idHeader.className).toEqual('pe-1 ps-1 bg-black text-white')
   })
 
   it('renders colummns when not selected', () => {
@@ -43,10 +46,14 @@ describe('DataHeaderRow', () => {
     )
 
     const addressHeader = screen.getAllByRole('columnheader')[0]
+    const idHeader = screen.getAllByRole('columnheader')[1]
+
     expect(addressHeader.className).toEqual('pe-1 ps-1 bg-blue-300 text-black')
+    expect(idHeader.className).toEqual('pe-1 ps-1 bg-blue-300 text-black')
 
     addressHeader.click()
 
     expect(addressHeader.className).toEqual('pe-1 ps-1 bg-black text-white')
+    expect(idHeader.className).toEqual('pe-1 ps-1 bg-blue-300 text-black')
   })
 })
