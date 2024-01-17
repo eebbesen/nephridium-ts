c58a71f8d0d5b47b0479f9b0b5ecff9734876d37
eebbesen <eebbesen.git@gmail.com>
2024-01-17 15:33:31 -0600

    Add columns back to the the date column selector in the same order they appear in the header row

--- __tests__/app/ui/dataHeaderRow.test.tsx
+++ __tests__/app/ui/dataHeaderRow.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import DataHeaderRow from '../../../src/app/ui/dataHeaderRow'
+import { addDateColumnOption } from '../../../src/app/ui/dataHeaderRow'
 import '@testing-library/jest-dom'
 
 let selected = []
@@ -9,6 +10,26 @@ function setSelected(slctd: string[]): void {
 }
 
 describe('DataHeaderRow', () => {
+  describe('addDateColumnOption', () => {
+    it('adds value in the table header order', () => {
+      const columns = ['id', 'address', 'date']
+      const selected = ['id', 'date']
+      const added = 'address'
+
+      expect(addDateColumnOption(added, selected, columns)).toEqual(['id', 'address', 'date'])
+      expect(selected.length).toEqual(2)
+    })
+
+    it('adds value when there are no others selected', () => {
+      const columns = ['id', 'address', 'date']
+      const selected: string[] = []
+      const added = 'address'
+
+      expect(addDateColumnOption(added, selected, columns)).toEqual(['address'])
+      expect(selected.length).toEqual(0)
+    })
+  })
+
   it('renders nothing with no colummns', () => {
     render(<DataHeaderRow columns={[]} />)
 
--- src/app/ui/dataHeaderRow.tsx
+++ src/app/ui/dataHeaderRow.tsx
@@ -1,5 +1,14 @@
 const transformer = require('../lib/util/transformer')
 
+// place the added value back in the dropdown at the appropriate index
+export function addDateColumnOption(added: string, selected: string[], columns: string[]): string[] {
+  const i: number = columns.indexOf(added)
+  let spliced = [...selected]
+  spliced.splice(i, 0, added)
+
+  return spliced
+}
+
 export default function DataHeaderRow({ columns, selected, setSelected }: any) {
   if (columns.length === 0) {
     return
@@ -10,7 +19,7 @@ export default function DataHeaderRow({ columns, selected, setSelected }: any) {
   const handleClick = (target: string, e: any): void => {
     const i: number = selected.indexOf(target)
     if (i < 0) {
-      setSelected([...selected, target])
+      setSelected(addDateColumnOption(target, selected, columns))
       e.target.classList.replace('text-black', 'text-white')
       e.target.classList.replace('bg-blue-300', 'bg-black')
     } else {
