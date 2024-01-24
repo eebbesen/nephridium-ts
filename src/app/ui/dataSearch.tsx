0c186d940c0e85b61dc59d494f5885505eb1571d
eebbesen <eebbesen.git@gmail.com>
2024-01-24 12:31:40 -0600

    Not currently using Vendor so removing

--- src/app/ui/dataSearch.tsx
+++ src/app/ui/dataSearch.tsx
@@ -10,7 +10,7 @@ export default function DataSearch({
   setWorkingUrl: (val: string) => void
 }) {
   return (
-    <div id="search-components" className="space-y-3">
+    <div id="search-components">
       <div id="search-url" className="grid gap-2 sm:grid-cols-2 md:grid-cols-9">
         <button
           id="search-button"
@@ -36,12 +36,6 @@ export default function DataSearch({
       <fieldset id="search-source">
         <div>
           <div className="grid justify-center sm:grid-cols-4 lg:grid-cols-9">
-            <legend className="col-start-1 col-end-2 text-center">
-              Vendor
-            </legend>
-            <Vendor vendor={'ArcGIS'} />
-            <Vendor vendor={'Socrata'} />
-            {/* <Vendor vendor={'Raw'} /> */}
           </div>
         </div>
       </fieldset>
